cf2554e9792b0db2d2e71b48d072fc8d72b5e370
pradeep sharma <pradeepsharmaghz@hotmail.com>
2026-02-09 15:02:28 +0000

    Adding maven project

---
+++ src/main/java/xyz/shrutideep/project/App.java
@@ -0,0 +1,10 @@
+package xyz.shrutideep.project;
+
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "A successfully built Java application using Maven" );
+    }
+}
